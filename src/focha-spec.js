3faa465f1b4c28693c7a3cb4f65be0a5368b9c43
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-06-06 21:57:50 -0400

    first commit

---
+++ src/focha-spec.js
@@ -0,0 +1,11 @@
+'use strict'
+
+
+/* global describe, it */
+const focha = require('.')
+
+describe('focha', () => {
+  it('write this test', () => {
+    console.assert(focha, 'should export something')
+  })
+})
